9363f54c141b01b37a8faf6a4aa1338747abfda6
tooney92 <tooneynta92@gmail.com>
2021-09-07 12:25:01 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => res.send('Hello World from tony!'));
+app.get('/sample', (req, res) => res.send('Hello form sample route'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
